4ab7d433e465f38f17b90cb705fc6c4276e3f5f5
ooKobi <oBloopPro@gmail.com>
2025-01-12 15:43:08 +0100

    1

--- docs/Wiki/JS/Navigate.js
+++ docs/Wiki/JS/Navigate.js
@@ -31,13 +31,13 @@ function LoadPageStyles(_page) {
     // Créer un élément link pour l'icône favicon
     var faviconLink = document.createElement("link");
     faviconLink.rel = "icon";
-    faviconLink.href = front + "IMG/ICO/ICO.ico";
+    faviconLink.href = front + "Wiki/IMG/ICO/ICO.ico";
 
     // Créer un élément link pour la feuille de style
     var cssLink = document.createElement("link");
     cssLink.rel = "stylesheet";
     cssLink.type = "text/css";
-    cssLink.href = front + "CSS/" + _page + ".css";
+    cssLink.href = front + "Wiki/CSS/" + _page + ".css";
     
     // Ajouter l'élément link au head de la page
     document.head.appendChild(cssLink);
